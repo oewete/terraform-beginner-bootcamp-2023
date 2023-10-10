3d613e4104438682f85fc3349e14210da224ec10
oewete <oewete@gmail.com>
2023-10-10 15:36:45 +0000

    #28 refactor s3 bucket into the new terrahouse module

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.bucket_name
+terraform{
 
-  tags = {
-    UserUuid = var.user_uuid
-  }
 }
-
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
+}
