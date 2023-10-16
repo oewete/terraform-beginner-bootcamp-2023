0b934628416aae1b4ec8e005e99f7ecec0dfaacf
oewete <oewete@gmail.com>
2023-10-16 22:00:16 +0000

    removed index & error variables

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -20,7 +20,7 @@ variable "bucket_name" {
   }
 }
 
-variable "index_html_filepath" {
+/* variable "index_html_filepath" {
   description = "The file path for index.html"
   type        = string
 
@@ -28,9 +28,9 @@ variable "index_html_filepath" {
     condition     = fileexists(var.index_html_filepath)
     error_message = "The provided path for index.html does not exist."
   }
-}
+} */
 
-variable "error_html_filepath" {
+/* variable "error_html_filepath" {
   description = "The file path for error.html"
   type        = string
 
@@ -38,4 +38,4 @@ variable "error_html_filepath" {
     condition     = fileexists(var.error_html_filepath)
     error_message = "The provided path for error.html does not exist."
   }
-}
+} */
