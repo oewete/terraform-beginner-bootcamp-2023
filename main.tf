519cb4bcbaeac7911943117d580aa245757f56cc
oewete <125014182+oewete@users.noreply.github.com>
2023-10-18 21:08:32 +0100

    45 terratowns provider (#48)
    * #45 wip setting up the skeleton for creating a custom terraform provider
    
    * #45 implement terraform provider block in terraform
    
    * #45 setup skeleton for resource

--- main.tf
+++ main.tf
@@ -24,12 +24,16 @@ provider "terratowns" {
 #}
 
 
-/* module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  assets_path = var.assets_path
-  content_version = var.content_version
-} */
+resource "terratowns_home" "home" {
+  name = "How to play Arcanum in 2023!"
+  description = <<DESCRIPTION
+Arcanum is a game from 2001 that shipped with alot of bugs.
+Modders have removed all the originals making this game really fun
+to play (despite that old look graphics). This is my guide that will
+show you how to play arcanum without spoiling the plot.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
